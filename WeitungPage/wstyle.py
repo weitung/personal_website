31c6a343942bd4dc9241590d365a4e8ada274810
Ubuntu Nexuni Server <ubuntu@ip-172-17-3-177.ap-southeast-1.compute.internal>
2022-10-06 00:33:25 +0000

    workable version

--- WeitungPage/wstyle.py
+++ WeitungPage/wstyle.py
@@ -3,7 +3,7 @@ from flask import render_template
 
 class WeitungPersonalPageStyle(Famcy.FamcyStyle):
 	def __init__(self, title=""):
-		super(WeitungPersonalPageStyle, self).__init__()
+		super(WeitungPersonalPageStyle, self).__init__(sijax_enable=True)
 
 		# set default value
 		self.title = title
@@ -11,8 +11,8 @@ class WeitungPersonalPageStyle(Famcy.FamcyStyle):
 
 		self.loader = Famcy.FamcyStyleLoader()
 		self.color_theme = Famcy.FamcyColorTheme()
-		self.nav_bar = Famcy.FamcyStyleNavBtns()
 		self.font_theme = Famcy.FamcyFontTheme()
+		self.nav_bar = Famcy.FamcyStyleNavBtns()
 		self.font_theme.setFontFamily("""-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", Helvetica, sans-serif""")
 
 	def setHeadScript(self, title=None, desc=None):
@@ -26,9 +26,23 @@ class WeitungPersonalPageStyle(Famcy.FamcyStyle):
 		html_header = self.setDashboardHTMLHeader()
 		end_js = end_script+self.setDashboardJavaScript()
 		color_theme = self.color_theme.render()
-		load_spinner = self.loader.render()
 		font_theme = self.font_theme.render()
-		# nav_bar = self.nav_bar.render()
+		load_spinner = self.loader.render()
+
+		nav_bar = self.nav_bar.render()
 
 		body_on_load = "background_loop('" + self.main_url + str(route) + "/bgloop" + "', '" + str(route) + "', " + str(time) + ");console.log('start!')" if background_flag else ""
-		return render_template("portfolio.html", form_init_js=form_init_js, load_spinner=load_spinner, font_theme=font_theme, color_theme=color_theme, html_header=html_header, content=content, extra_script=extra_script, end_js=end_js, body_on_load=body_on_load)
+		if kwargs["event_source_flag"]:
+			_event_source_script = '''
+			var source = new EventSource("/event_source");
+		    source.addEventListener('publish', function(event) {
+		        var data = JSON.parse(event.data);
+		        update_event_source_target(data)
+		    }, false);
+		    source.addEventListener('error', function(event) {
+		        console.log("Error"+ event)
+		    }, false);
+			'''
+		else:
+			_event_source_script = ''
+		return render_template("portfolio.html", event_source=_event_source_script, font_theme=font_theme, form_init_js=form_init_js, nav_bar=nav_bar, load_spinner=load_spinner, color_theme=color_theme, html_header=html_header, content=content, extra_script=extra_script, end_js=end_js, body_on_load=body_on_load)
