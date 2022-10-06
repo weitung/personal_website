078af33e39e7111c821ab4cacf7523be12356407
Ubuntu Nexuni Server <ubuntu@ip-172-17-3-177.ap-southeast-1.compute.internal>
2022-10-06 00:00:04 +0000

    update stuffs

--- WeitungPage/wstyle.py
+++ WeitungPage/wstyle.py
@@ -12,6 +12,8 @@ class WeitungPersonalPageStyle(Famcy.FamcyStyle):
 		self.loader = Famcy.FamcyStyleLoader()
 		self.color_theme = Famcy.FamcyColorTheme()
 		self.nav_bar = Famcy.FamcyStyleNavBtns()
+		self.font_theme = Famcy.FamcyFontTheme()
+		self.font_theme.setFontFamily("""-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", Helvetica, sans-serif""")
 
 	def setHeadScript(self, title=None, desc=None):
 		if title:
@@ -25,8 +27,8 @@ class WeitungPersonalPageStyle(Famcy.FamcyStyle):
 		end_js = end_script+self.setDashboardJavaScript()
 		color_theme = self.color_theme.render()
 		load_spinner = self.loader.render()
-
+		font_theme = self.font_theme.render()
 		# nav_bar = self.nav_bar.render()
 
 		body_on_load = "background_loop('" + self.main_url + str(route) + "/bgloop" + "', '" + str(route) + "', " + str(time) + ");console.log('start!')" if background_flag else ""
-		return render_template("portfolio.html", form_init_js=form_init_js, load_spinner=load_spinner, color_theme=color_theme, html_header=html_header, content=content, extra_script=extra_script, end_js=end_js, body_on_load=body_on_load)
+		return render_template("portfolio.html", form_init_js=form_init_js, load_spinner=load_spinner, font_theme=font_theme, color_theme=color_theme, html_header=html_header, content=content, extra_script=extra_script, end_js=end_js, body_on_load=body_on_load)
